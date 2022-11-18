254dc22193ebb52ebfbf18ce486efddc356b74ca
Kyrian <dr.kyrian.onwe@gmail.com>
2022-11-17 19:52:47 -0600

    commit tree 79387f0041534cca38f3d0a2593583b6fcf6c397

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+    validates :name, presence: true
+    validates :name, length: { minimum: 1, too_short: "%{count} characters is the minimum for name" }
+    validates :name, uniqueness: { case_sensitivity: true}
+    validates :phone_number, length: {is: 10}
 end
